39dc4e66c9aa1a74a17082810ba02e00c29811bd
OlehPronQA <oleg.pron.000@gmail.com>
2024-02-03 21:55:26 +0200

    hw-js-1

---
+++ Task#6-js-hw-1.js
@@ -0,0 +1,23 @@
+// 6.  Задано номер дня тижня.
+// За заданим номером вивести назву дня тижня використовуючи switch.
+// Результат виводити в консоль.
+
+let week = 0;
+
+switch (week) {
+  case 1:
+    console.log("week = 1");
+    break;
+  case 2:
+    console.log("week = 2");
+    break;
+  case 3:
+    console.log("week = 3");
+    break;
+  case 4:
+    console.log("week = 4");
+    break;
+  default:
+    console.log("there is no such week");
+    break;
+}
---
+++ Task#7-js-hw-1.js
@@ -0,0 +1,24 @@
+// 7.  За допомогою switch реалізуйте обчислення виразу, передаючи у switch, як параметр, символ математичної операції.
+//     Математичні операції для обчислення: "+", "-", "*", "/".
+//     Результат виводити в консоль.
+
+let a = 10;
+let b = 993;
+let operator = "/";
+
+switch (operator) {
+  case "+":
+    console.log(a + b);
+    break;
+  case "-":
+    console.log(a - b);
+    break;
+  case "*":
+    console.log(a * b);
+    break;
+  case "/":
+    console.log(a / b);
+    break;
+  default:
+    console.log("невідома операція");
+}
